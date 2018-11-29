a3e5f8237db7c01e829ea53efba7a9a3bf4f73a8
Abraham¬_¬ <AbrahamRiRa>
2018-11-29 12:48:40 -0600

    Se agrego el primer metodo para busqueda BUSQUEDA SECUENCIAL

---
+++ BusquedaPython.py
@@ -0,0 +1,17 @@
+'''
+Created on 29 nov 2018
+
+@author: Abraham¬_¬
+'''
+from imaplib import dat
+class Busquedas:
+    def busquedaSecuencial(self,lista,dat):
+        pos=0
+        encontrado=False
+        while pos<len(lista) and not encontrado:
+            if list[pos]==dat:
+            encontrado=True
+            else:
+                 pos=pos+1
+        
+    
